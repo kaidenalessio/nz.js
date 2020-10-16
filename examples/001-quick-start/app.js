c4ac5ea9f7493364dcad66cfaec57d41992d0006
kaiden <kaidenalessio@gmail.com>
2020-10-16 13:12:47 +0700

    Update example 001

--- examples/001-quick-start/app.js
+++ examples/001-quick-start/app.js
@@ -2,23 +2,23 @@ let angle;
 let angVel;
 let angAcc;
 
-Room.current.start = () => {
+Scene.current.start = () => {
 	angle = 0;
 	angVel = 2;
 	angAcc = 0;
 };
 
-Room.current.update = () => {
+Scene.current.update = () => {
 	angAcc = Input.mouseWheelUp() - Input.mouseWheelDown() + Input.keyHold(KeyCode.W) - Input.keyHold(KeyCode.S);
 	angVel += angAcc;
 	angle += angVel;
 	if (Math.abs(angVel) > 2) {
 		angVel *= 0.99;
 	}
-	Room.autoClear = Input.keyHold(KeyCode.Space);
+	Stage.autoClear = Input.keyHold(KeyCode.Space);
 };
 
-Room.current.render = () => {
+Scene.current.render = () => {
 	if (Input.mouseHold(0)) {
 		Draw.setColor(C.pink);
 		if (Input.mouseMove) {
@@ -36,14 +36,14 @@ Room.current.render = () => {
 	Draw.roundRectRotated(Input.mouseX, Input.mouseY, 32, 32, 5, angle, true);
 };
 
-Room.current.renderUI = () => {
+Scene.current.renderUI = () => {
 	Draw.setColor(C.white);
 	Draw.roundRect(-10, -10, 280, 225);
 	Draw.textBackground(0, 10, `FPS: ${Time.FPS}`);
 	Draw.textBackground(0, 40, `Angle: ${angle.toFixed(2)}`);
 	Draw.textBackground(0, 70, `Angle velocity: ${angVel.toFixed(2)}`);
 	Draw.textBackground(0, 100, `Angle acceleration: ${angAcc}`);
-	Draw.textBackground(0, 130, `Auto clear (hold space): ${Room.autoClear}`);
+	Draw.textBackground(0, 130, `Auto clear (hold space): ${Stage.autoClear}`);
 	Draw.textBackground(0, 160, `Hold w-key or s-key or scroll\nmouse wheel to accelerate.`);
 };
 
