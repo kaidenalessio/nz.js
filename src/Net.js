621f06ca9c6878f96767e702bbb03a64e3ef6e03
kaiden <kaidenalessio@gmail.com>
2020-10-18 12:07:14 +0700

    Net 2.0, Allow custom node

--- src/Net.js
+++ src/Net.js
@@ -1,6 +1,38 @@
 var NZ = NZ || {};
 
+class NZNetBuffers {
+	constructor(length=10) {
+		this.b = [];
+		this.length = length;
+		for (let i = 0; i < this.length; i++) {
+			this.b.push([]);
+		}
+	}
+	get(i) {
+		return this.b[i];
+	}
+	push(i, value) {
+		this.b[i].push(value);
+	}
+	pop(i) {
+		return this.b[i].shift();
+	}
+	clear(i) {
+		this.b[i].length = 0;
+	}
+	copy(destBufferIndex, sourceBuffer) {
+		const a = this.b[destBufferIndex];
+		const b = sourceBuffer;
+		a.length = 0;
+		for (let i = b.length - 1; i >= 0; --i) {
+			a.unshift(b[i]);
+		}
+		return a;
+	}
+}
+
 // Google firebase wrapper
+// Requires NZNetBuffers class
 // SCRIPTS REQUIRED:
 /*
  <script src="https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js"></script>
@@ -13,79 +45,105 @@ NZ.Net = {
 	// dummy message, if you ever want to just send certain buffer
 	// but not actually have any value to send, just send this dummy
 	DUMMY: 0,
-	bufferAmount: 10,
-	buffers: [],
-	database: null,
-	databaseName: 'NZBuffers',
-	init(firebaseConfig, databaseName, bufferAmount) {
+	parentPath: 'NZ/',
+	fbDatabase: null,
+	connections: {}, // built in buffer and listener manager
+	init(firebaseConfig) {
 		firebase.initializeApp(firebaseConfig);
-		this.databaseName = databaseName || this.databaseName;
-		this.bufferAmount = bufferAmount || this.bufferAmount;
-		this.database = firebase.database();
-		for (let i = 0; i < this.bufferAmount; i++) {
-			this.database.ref(`${this.databaseName}/${i}`).on('value', this.onValueEvent);
-		}
+		this.fbDatabase = firebase.database();
 	},
-	onValueEvent(snapshot) {
-		const key = +snapshot.key;
-		const val = snapshot.val();
+	onValueEvent(node, snapshot) {
+		const key = snapshot.key; // expected buffer index
+		const val = snapshot.val(); // expected buffer (array of values)
 		// is there any value received
 		if (val) {
 			// is it an array
 			if (val.length) {
-				// copy array val to buffer[key]
-				NZ.Net.clearBuffer(key);
-				for (let i = 0; i < val.length; i++) {
-					NZ.Net.push(key, val[i]);
-				}
+				// copy it to local buffer
+				NZ.Net.copyBuffer(node, +key, val); // +key (convert to number type)
 			}
 		}
 	},
-	clearBuffer(i) {
-		this.buffers[i].length = 0;
+	startListening(path, callbackFn) {
+		// Returns a listener u can use to stop listening
+		return this.fbDatabase.ref(path).on('value', callbackFn);
 	},
-	push(i, value) {
-		this.buffers[i].push(value);
+	stopListening(path, listener) {
+		this.fbDatabase.ref(path).off('value', listener);
 	},
-	sendBuffer(i) {
-		this.database.ref(`${this.databaseName}/${i}`).set(this.buffers[i]);
+	createConnection(node) {
+		const path = `${this.parentPath}${node}`;
+		this.connections[node] = {};
+		this.connections[node].buffers = new NZNetBuffers();
+		this.connections[node].listeners = [];
+		for (let i = 0; i < this.connections[node].buffers.length; i++) {
+			this.connections[node].listeners.push(this.startListening(`${path}/${i}`, (snapshot) => { this.onValueEvent(node, snapshot); }));
+		}
 	},
-	getBuffer(i) {
-		return this.buffers[i];
+	destroyConnection(node) {
+		const path = `${this.parentPath}${node}`;
+		for (let i = this.connections[node].listeners.length - 1; i >= 0; --i) {
+			const listener = this.connections[node].listeners[i];
+			this.stopListening(`${path}/${i}`, listener);
+		}
+		delete this.connections[node];
 	},
-	pop(i) {
-		// Removes then returns the first element of buffers[`i`]
-		return this.buffers[i].shift();
+	connectionExists(node) {
+		return this.connections[node] !== undefined;
 	},
-	send(i, ...payload) {
-		this.clearBuffer(i);
+	copyBuffer(node, i, sourceBuffer) {
+		this.connections[node].buffers.copy(i, sourceBuffer);
+	},
+	clearBuffer(node, i) {
+		this.connections[node].buffers.clear(i);
+	},
+	push(node, i, value) {
+		this.connections[node].buffers.push(i, value);
+	},
+	getBuffer(node, i) {
+		return this.connections[node].buffers.get(i);
+	},
+	sendBuffer(node, i) {
+		const path = `${this.parentPath}${node}/${i}`;
+		this.fbDatabase.ref(path).set(this.getBuffer(node, i));
+	},
+	pop(node, i) {
+		return this.connections[node].buffers.pop(i);
+	},
+	sendUnsafe(node, i, ...payload) {
+		this.clearBuffer(node, i);
 		for (const p of payload) {
-			this.push(i, p);
+			this.push(node, i, p);
 		}
-		this.sendBuffer(i);
+		this.sendBuffer(node, i);
 	},
-	read(i, callbackFn) {
-		// read through the buffer[i], executes callback, then clear the buffer[i]
-		// u may use Net.pop(i); in callbackFn to retrieve value
-		while (this.getBuffer(i).length > 0) {
-			callbackFn();
-			this.clearBuffer(i);
-		}
-	},
-	clearBuffers() {
-		// clear buffers
-		this.buffers.length = 0;
-		// create/recreate buffers
-		for (let i = 0; i < this.bufferAmount; i++) {
-			this.buffers.push([]);
+	readUnsafe(node, i, callbackFn) {
+		// read through the buffer, executes callback
+		// u may use pop(); in callbackFn to retrieve value
+		// warning: if u dont pop() while will loop forever
+		const pop = () => this.pop(node, i);
+		while (this.getBuffer(node, i).length > 0) {
+			callbackFn(pop);
 		}
 	},
-	sendEmptyBuffers() {
-		for (let i = 0; i < this.bufferAmount; i++) {
-			this.clearBuffer(i);
-			this.sendBuffer(i);
+	send(node, i, ...payload) {
+		if (!this.connectionExists(node)) return false;
+		this.sendUnsafe(node, i, ...payload);
+		this.clearBuffer(node, i);
+		return true;
+	},
+	read(node, i, callbackFn) {
+		if (!this.connectionExists(node)) return false;
+		this.readUnsafe(node, i, (pop) => {
+			callbackFn(pop);
+			this.clearBuffer(node, i);
+		});
+		return true;
+	},
+	sendEmptyBuffers(node) {
+		for (let i = this.connections[node].buffers.length - 1; i >= 0; --i) {
+			this.clearBuffer(node, i);
+			this.sendBuffer(node, i);
 		}
 	}
 };
-
-NZ.Net.clearBuffers();
