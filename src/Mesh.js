f2cf9e8f063ca6490aa391332c4b32c230bccac3
kaiden <kaidenalessio@gmail.com>
2020-10-20 13:11:47 +0700

    Add Mesh.applyTransform and Mesh.onAllTris

--- src/Mesh.js
+++ src/Mesh.js
@@ -6,6 +6,13 @@ NZ.Mesh = function(tris=[]) {
 	this.tris = tris;
 }
 
+NZ.Mesh.prototype.onAllTris = function(fn) {
+	const n = this.tris.length;
+	for (let i = 0; i < n; i++) {
+		fn(this.tris[i]);
+	}
+};
+
 NZ.Mesh.prototype.loadFromOBJText = function(objText) {
 	this.tris.length = 0;
 	const vertices = [];
@@ -15,6 +22,19 @@ NZ.Mesh.prototype.loadFromOBJText = function(objText) {
 		switch (words.shift()) {
 			case 'v': vertices.push(new NZ.Vec3(get(), get(), get())); break;
 			case 'f': this.tris.push(new NZ.Tri([vertices[get()-1], vertices[get()-1], vertices[get()-1]])); break;
+		}
+	}
+};
+
+NZ.Mesh.applyTransform = function(mesh, transform) {
+	const matWorld = NZ.Mat4.makeTransformation(transform);
+	const vertices = [];
+	for (const tri of mesh.tris) {
+		for (const p of tri.p) {
+			if (!vertices.includes(p)) {
+				p.set(NZ.Mat4.multVec3(matWorld, p));
+				vertices.push(p);
+			}
 		}
 	}
 };
