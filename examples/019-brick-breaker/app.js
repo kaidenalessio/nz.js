55f9fa06a50813a77b4c4d555a7a46ab8df294c5
kaiden <kaidenalessio@gmail.com>
2020-10-21 14:57:11 +0700

    Add paddle

--- examples/019-brick-breaker/app.js
+++ examples/019-brick-breaker/app.js
@@ -1,4 +1,3 @@
-const rectContainsPoint = (b, p) => p.x >= b.x && p.x <= b.x + b.w && p.y >= b.y && p.y <= b.y + b.h;
 const rectIntersectsCircle = (b, c) => c.x + c.r >= b.x && c.x - c.r <= b.x + b.w && c.y + c.r >= b.y && c.y - c.r <= b.y + b.h;
 
 const BRICK_COLORS = [
@@ -7,17 +6,26 @@ const BRICK_COLORS = [
 	C.salmon
 ];
 
+let BALL_COUNT = 3;
+let BALL_COLOR = C.mediumSpringGreen;
+let BALL_SPEED = 10;
+let BALL_RADIUS = 10;
+let BALL_SPAWN_ANGLE = 270;
+
+let PADDLE_W = 100;
+let PADDLE_H = 10;
+let PADDLE_SPD = 10;
+
 let GAME_OVER = false;
 let GAME_OVER_TEXT = '';
 
-class Brick extends NZObject {
-	constructor(x, y, w, h, level) {
+class Block extends NZObject {
+	constructor(x, y, w, h) {
 		super();
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
-		this.level = level;
 	}
 	get left() {
 		return this.x;
@@ -31,57 +39,76 @@ class Brick extends NZObject {
 	get bottom() {
 		return this.y + this.h;
 	}
+	get center() {
+		return this.x + this.w * 0.5;
+	}
+	get middle() {
+		return this.y + this.h * 0.5;
+	}
+	drawSelf() {
+		Draw.rect(this.x, this.y, this.w, this.h);
+	}
+}
+
+class Brick extends Block {
+	constructor(x, y, w, h, level) {
+		super(x, y, w, h);
+		this.level = level;
+	}
 	get colorIndex() {
 		return Mathz.clamp(this.level - 1, 0, BRICK_COLORS.length - 1);
 	}
 	intersectsCircle(c) {
 		return rectIntersectsCircle(this, c);
 	}
-	hit() {
+	hit(b) {
 		this.level -= 1;
 		if (this.level <= 0) {
 			OBJ.remove(this.id);
 		}
 		// game over check
-		if (OBJ.count('Brick') <= 0) {
-			GAME_OVER = true;
-			GAME_OVER_TEXT = 'You won!';
+		if (!GAME_OVER) {
+			if (OBJ.count('Brick') <= 0) {
+				GAME_OVER = true;
+				GAME_OVER_TEXT = 'You won!';
+			}
 		}
 	}
 	render() {
 		Draw.setColor(BRICK_COLORS[this.colorIndex]);
-		Draw.rect(this.x, this.y, this.w, this.h);
+		this.drawSelf();
 	}
 }
 
 class Ball extends NZObject {
 	constructor(x, y) {
 		super();
-		this.r = 10;
+		this.c = C.mediumSpringGreen;
+		this.r = BALL_RADIUS;
 		this.x = x;
 		this.y = y;
 		this.vx = 0;
 		this.vy = 0;
-		this.bounds = {
-			left: this.x - this.r,
-			right: this.x + this.r,
-			top: this.y - this.r,
-			bottom: this.y + this.r
-		};
+	}
+	get left() {
+		return this.x - this.r;
+	}
+	get right() {
+		return this.x + this.r;
+	}
+	get top() {
+		return this.y - this.r;
+	}
+	get bottom() {
+		return this.y + this.r;
 	}
 	applyForce(v) {
 		this.vx = v.x;
 		this.vy = v.y;
 	}
-	updateBound() {
+	updateMovement() {
 		this.xprev = this.x;
 		this.yprev = this.y;
-		this.bounds.left = this.x - this.r;
-		this.bounds.right = this.x + this.r;
-		this.bounds.top = this.y - this.r;
-		this.bounds.bottom = this.y + this.r;
-	}
-	updateMovement() {
 		this.x += this.vx;
 		this.y += this.vy;
 	}
@@ -90,11 +117,34 @@ class Ball extends NZObject {
 			if (brick.intersectsCircle(this)) {
 				this.x = this.xprev;
 				this.y = this.yprev;
-				const lookAngle = Vec2.direction(Vec2.fromObject(this), Vec2.fromObject(brick));
-				const v = Vec2.polar(Mathz.range(lookAngle + 90, lookAngle + 270), Vec2.create(this.vx, this.vy).length);
+				let lookAngle = Vec2.direction(Vec2.fromObject(this), new Vec2(brick.center, brick.middle));
+				lookAngle += 180;
+				const v = Vec2.polar(Mathz.range(lookAngle - 90, lookAngle + 90), BALL_SPEED);
 				this.vx = v.x;
 				this.vy = v.y;
-				brick.hit();
+				brick.hit(this);
+			}
+		}
+		for (const paddle of OBJ.take('Paddle')) {
+			if (this.bottom >= paddle.top && this.bottom < paddle.bottom && this.right >= paddle.left && this.left <= paddle.right) {
+				this.y = paddle.y - this.r;
+				this.vy = -this.vy;
+				// if ball moving left
+				if (this.vx < 0) {
+					// and touch the right part of paddle
+					if (this.x > paddle.center) {
+						// move right
+						this.vx = -this.vx;
+					}
+				}
+				// if ball moving right
+				if (this.vx > 0) {
+					// and touch the left part of paddle
+					if (this.x < paddle.center) {
+						// move left
+						this.vx = -this.vx;
+					}
+				}
 			}
 		}
 	}
@@ -111,29 +161,54 @@ class Ball extends NZObject {
 			this.y = this.r;
 			this.vy = -this.vy;
 		}
-		if (this.y + this.r >= Stage.h) {
-			this.y = Stage.h - this.r;
-			this.vy = -this.vy;
+		if (this.y - this.r >= Stage.h) {
+			this.kill();
 		}
 	}
 	update() {
-		this.updateBound();
 		this.updateMovement();
 		this.updateCollision();
 		this.updateStageCollision();
 	}
 	render() {
-		Draw.setColor(C.mediumSpringGreen);
+		Draw.setColor(this.c);
 		Draw.circle(this.x, this.y, this.r);
+	}
+	kill() {
+		OBJ.remove(this.id);
+		// game over check
+		if (!GAME_OVER) {
+			if (BALL_COUNT <= 0) {
+				GAME_OVER = true;
+				GAME_OVER_TEXT = 'Out of ball';
+			}
+		}
 	}
 }
 
-OBJ.addLink('Ball', Ball);
+class Paddle extends Block {
+	constructor(x, y) {
+		super(x, y, PADDLE_W, PADDLE_H);
+		this.x -= this.w * 0.5;
+	}
+	update() {
+		this.x += PADDLE_SPD * (Input.keyHold(KeyCode.Right) - Input.keyHold(KeyCode.Left));
+		this.x = Mathz.clamp(this.x, 0, Stage.w - this.w);
+	}
+	render() {
+		Draw.setColor(C.white);
+		this.drawSelf();
+	}
+}
+
+OBJ.addLink('Paddle', Paddle);
 OBJ.addLink('Brick', Brick);
+OBJ.addLink('Ball', Ball);
 
 Scene.current.start = () => {
-	const n = OBJ.create('Ball', Stage.mid.w, Stage.h - 100);
-	n.applyForce(Vec2.polar(Mathz.range(190, 350), 10));
+	BALL_COUNT = 3;
+	GAME_OVER = false;
+	OBJ.create('Paddle', Stage.mid.w, Stage.h - 50);
 	const brick = {
 		w: 60,
 		h: 15,
@@ -155,6 +230,34 @@ Scene.current.renderUI = () => {
 	if (GAME_OVER) {
 		Draw.setFont(Font.xxl);
 		Draw.textBG(Stage.mid.w, Stage.mid.h, GAME_OVER_TEXT, { origin: Vec2.center });
+		Draw.setFont(Font.m);
+		Draw.textBG(Stage.mid.w, Stage.h - 22, 'Press space to restart', { origin: Vec2.center });
+		return;
+	}
+	Utils.repeat(BALL_COUNT, (i) => {
+		Draw.setColor(BALL_COLOR);
+		Draw.circle(Stage.mid.w + (i-BALL_COUNT*0.5+0.5) * 40, Stage.h - 20, 8);
+	});
+	if (OBJ.count('Ball') < 1) {
+		BALL_SPAWN_ANGLE = 270 + 80 * Math.sin(Time.time * 0.01);
+		for (const paddle of OBJ.take('Paddle')) {
+			if (Input.keyDown(KeyCode.Space)) {
+				const n = OBJ.create('Ball', paddle.center, paddle.y - BALL_RADIUS);
+				n.applyForce(Vec2.polar(BALL_SPAWN_ANGLE, BALL_SPEED));
+				BALL_COUNT--;
+			}
+			const p = {
+				x: paddle.center,
+				y: paddle.y - BALL_RADIUS
+			};
+			Draw.setFont(Font.m);
+			Draw.textBG(Stage.mid.w, Stage.mid.h, 'Press space to spawn ball', { origin: Vec2.center });
+			Draw.setColor(C.black, BALL_COLOR);
+			Draw.circle(p.x, p.y, BALL_RADIUS);
+			Draw.stroke();
+			const p1 = Vec2.polar(BALL_SPAWN_ANGLE, BALL_RADIUS).add(p);
+			Draw.pointLine(p, p1);
+		}
 	}
 };
 
