1f41a7bf167c51aafb7598b8b8111998f2a4e398
kaiden <kaidenalessio@gmail.com>
2020-10-30 02:08:56 +0700

    cleaned build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
-set nz_time_string=%time: =0%
-ren build\nz.js nz-backup_%date:~10,4%%date:~4,2%%date:~7,2%_%nz_time_string:~0,2%%nz_time_string:~3,2%%nz_time_string:~6,2%.js
-copy /b src\*.js+src\objects\*.js+Alias.js build\nz.js
+@echo off
+set nz_time_start=%time: =0%
+echo ### NZ BUILD START ###### %nz_time_start%
+echo Renaming...
+ren build\nz.js nz-backup_%date:~10,4%%date:~4,2%%date:~7,2%_%nz_time_start:~0,2%%nz_time_start:~3,2%%nz_time_start:~6,2%%nz_time_start:~9,2%.js
+echo Copying...
+copy /b src\*.js+src\objects\*.js+Alias.js build\nz.js >nul
+echo ### NZ BUILD COMPLETE ### %time: =0%
