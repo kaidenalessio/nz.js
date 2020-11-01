ee6e9f0d1e0e906dc25b7ac04874c3588265d720
kaiden <kaidenalessio@gmail.com>
2020-11-01 18:38:40 +0700

    add more built in font and setFamily function

--- build/nz.js
+++ build/nz.js
@@ -995,15 +995,32 @@ NZ.Font = {
 		for (const fontName of fontNames) {
 			document.head.appendChild(this.createGoogleFontLink(fontName));
 		}
+	},
+	setFamily(family) {
+		const keys = Object.keys(NZ.Font);
+		for (const key of keys) {
+			const value = NZ.Font[key];
+			if (typeof value === 'object') {
+				if (value.size !== undefined && value.style !== undefined && value.family !== undefined) {
+					NZ.Font[key].family = family;
+				}
+			}
+		}
 	}
 };
 
 NZ.Font.xxl = NZ.Font.generate(NZ.Font.h1); // { size: 48, style: '', family: 'Maven Pro, sans-serif' }
-NZ.Font.xl 	= NZ.Font.generate(NZ.Font.h2);
-NZ.Font.l 	= NZ.Font.generate(NZ.Font.h3);
-NZ.Font.m 	= NZ.Font.generate(NZ.Font.h4);
-NZ.Font.sm 	= NZ.Font.generate(NZ.Font.h5);
-NZ.Font.s 	= NZ.Font.generate(NZ.Font.h6);var NZ = NZ || {};
+NZ.Font.xl  = NZ.Font.generate(NZ.Font.h2);
+NZ.Font.l   = NZ.Font.generate(NZ.Font.h3);
+NZ.Font.m   = NZ.Font.generate(NZ.Font.h4);
+NZ.Font.sm  = NZ.Font.generate(NZ.Font.h5);
+NZ.Font.s   = NZ.Font.generate(NZ.Font.h6);
+NZ.Font.xxlb = NZ.Font.generate(NZ.Font.xxl.size, NZ.Font.bold, NZ.Font.xxl.family);
+NZ.Font.xlb  = NZ.Font.generate(NZ.Font.xl.size, NZ.Font.bold, NZ.Font.xl.family);
+NZ.Font.lb   = NZ.Font.generate(NZ.Font.l.size, NZ.Font.bold, NZ.Font.l.family);
+NZ.Font.mb   = NZ.Font.generate(NZ.Font.m.size, NZ.Font.bold, NZ.Font.m.family);
+NZ.Font.smb  = NZ.Font.generate(NZ.Font.sm.size, NZ.Font.bold, NZ.Font.sm.family);
+NZ.Font.sb   = NZ.Font.generate(NZ.Font.s.size, NZ.Font.bold, NZ.Font.s.family);var NZ = NZ || {};
 
 NZ.Input = {
 	targetElement: null,
