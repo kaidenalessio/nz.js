8da8968276b7a4584b576397ca312243b47dc7f7
kaiden <kaidenalessio@gmail.com>
2020-11-09 23:04:10 +0700

    mouse trail

--- examples/038-easing-and-tweening/app.js
+++ examples/038-easing-and-tweening/app.js
@@ -1,24 +1,41 @@
-let ease, target, position;
+let target,
+	points = [],
+	ease = 0.99;
+
+const easeTo = (position, target, ease, epsilon=0.01) => {
+	let dx = (target.x - position.x),
+		dy = (target.y - position.y);
+
+	position.x += dx * ease;
+	position.y += dy * ease;
+
+	return (Math.abs(dx) > epsilon || Math.abs(dy) > epsilon);
+};
 
 NZ.start({
 
 start() {
 	target = Input.mousePosition;
-	position = {
-		x: Stage.randomX,
-		y: Stage.randomY
-	};
-	ease = 0.1;
+	for (let i = 0; i < 500; i++) {
+		points.push({
+			x: Stage.randomX,
+			y: Stage.randomY
+		});
+	}
 },
 
 render() {
-	let dx = target.x - position.x,
-		dy = target.y - position.y;
+	let leader = target,
+		easing;
 
-	position.x += dx * ease;
-	position.y += dy * ease;
-
-	Draw.circle(position.x, position.y, 16);
+	Draw.setColor(C.black);
+	for (let i = 0; i < points.length; i++) {
+		easing = easeTo(points[i], leader, ease);
+		if (easing) {
+			Draw.pointCircle(points[i], (points.length - i) * 0.005);
+		}
+		leader = points[i];
+	}
 }
 
 });
