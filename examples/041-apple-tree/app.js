7f47a5e3d4d5261a4d92cefb0cbf24110b7866dd
kaiden <kaidenalessio@gmail.com>
2020-11-10 15:46:26 +0700

    more tweens, a little sound and bg image

--- examples/041-apple-tree/app.js
+++ examples/041-apple-tree/app.js
@@ -7,6 +7,8 @@ class Apple {
 		this.y = -Stage.h * 0.1;
 		this.lane = lane;
 		this.range = 30;
+		this.angle = Mathz.range(360);
+		this.scale = Mathz.range(0.8, 1);
 		this.imageName = imageName;
 		this.score = this.imageName === Apple.ROTTEN? -1 : 1;
 		this.updateX();
@@ -21,12 +23,12 @@ class Apple {
 		this.y += Apple.VSP;
 		let basket = OBJ.takeFrom('Basket')[0];
 		if (this.intersects(basket) && this.lane === basket.lane) {
-			basket.score += this.score;
+			basket.addScore(this.score);
 			OBJ.remove(this.id);
 		}
 	}
 	render() {
-		Draw.image(this.imageName, this.x, this.y);
+		Draw.imageTransformed(this.imageName, this.x, this.y, this.scale, this.scale, this.angle);
 		if (Debug.mode > 0)
 			Draw.circle(this.x, this.y, this.range, true);
 	}
@@ -39,10 +41,31 @@ class Basket {
 		this.lane = lane;
 		this.score = 0;
 		this.range = 40;
+		this.scale = 1;
+		this.scoreColor = {
+			r: 255,
+			g: 255,
+			b: 255
+		};
 		this.updateX();
 	}
 	updateX() {
 		Tween.tween(this, { x: Stage.mid.w + this.lane * LANE_W }, 30, Easing.ElasticEaseOut);
+	}
+	addScore(amount) {
+		Sound.play('eat');
+		this.score += amount;
+		this.scale = 1;
+		Tween.tween(this, { scale: 1.2 }, 5, Easing.BounceEaseOut)
+			 .chain(this, { scale: 1 }, 20, Easing.BounceEaseOut);
+
+		if (amount < 0) {
+			Tween.tween(this, { y: this.y + 10 }, 10, Easing.QuadEaseOut)
+				 .chain(this, { y: this.y }, 15, Easing.BackEaseOut);
+
+			Tween.tween(this.scoreColor, { g: 0, b: 0 }, 15, Easing.QuintEaseOut)
+				 .chain(this.scoreColor, { g: 255, b: 255 }, 30, Easing.QuintEaseIn);
+		}
 	}
 	moveLeft() {
 		if (this.lane > -1)
@@ -65,7 +88,7 @@ class Basket {
 			this.moveRight();
 	}
 	render() {
-		Draw.image('basket', this.x, this.y);
+		Draw.imageTransformed('basket', this.x, this.y, this.scale, this.scale, 0);
 		if (Debug.mode > 0)
 			Draw.circle(this.x, this.y, this.range, true);
 	}
@@ -76,9 +99,11 @@ OBJ.addLink('Basket', Basket);
 
 let spawnTime, LANE_W;
 
+Loader.loadImage(Vec2.center, 'bg', 'bg.png');
 Loader.loadImage(Vec2.center, 'fresh', 'fresh.png');
 Loader.loadImage(Vec2.center, 'basket', 'basket.png');
 Loader.loadImage(Vec2.center, 'rotten', 'rotten.png');
+Loader.loadSound('eat', 'eat.mp3');
 
 Font.setFamily('Montserrat Alternates, sans-serif');
 
@@ -101,23 +126,31 @@ start() {
 
 },
 
-render() {
-
+update() {
 	if (Time.frameCount > spawnTime) {
-		OBJ.create('Apple', Mathz.choose(Apple.FRESH, Apple.ROTTEN), Mathz.choose(-1, 0, 1));
+		OBJ.create('Apple', Mathz.choose(Apple.FRESH, Apple.FRESH, Apple.FRESH, Apple.ROTTEN), Mathz.choose(-1, 0, 1));
 		spawnTime = Time.frameCount + Mathz.range(20, 60);
 	}
 
 },
 
+render() {
+	Draw.image('bg', Stage.mid.w, Stage.mid.h);
+},
+
 renderUI() {
+	let basket = OBJ.takeFrom('Basket')[0],
+		c = basket.scoreColor;
+
 	Draw.setFont(Font.xxlb);
 	Draw.setHVAlign(Align.r, Align.t);
 	for (let i = 0; i < 2; i++) {
-		Draw.setFill(i > 0? C.white : C.black);
-		Draw.textRegular(Stage.w - 10 - i, 10 - 2*i, OBJ.takeFrom('Basket')[0].score);
+		Draw.setFill(i > 0? `rgb(${c.r}, ${c.g}, ${c.b})` : C.black);
+		Draw.textTransformed(Stage.w - 10 - i, 10 - 2*i, basket.score, basket.scale, basket.scale, 0);
 	}
 },
+
+stylePreset: StylePreset.noGapCenter,
 
 embedGoogleFonts: ['Montserrat Alternates']
 
