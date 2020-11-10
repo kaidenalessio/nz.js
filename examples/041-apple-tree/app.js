40d362245f1da19c0c405d04ffeda8f778bdfbf9
kaiden <kaidenalessio@gmail.com>
2020-11-10 15:25:31 +0700

    add images and score system

--- examples/041-apple-tree/app.js
+++ examples/041-apple-tree/app.js
@@ -8,6 +8,7 @@ class Apple {
 		this.lane = lane;
 		this.range = 30;
 		this.imageName = imageName;
+		this.score = this.imageName === Apple.ROTTEN? -1 : 1;
 		this.updateX();
 	}
 	intersects(other) {
@@ -19,15 +20,15 @@ class Apple {
 	update() {
 		this.y += Apple.VSP;
 		let basket = OBJ.takeFrom('Basket')[0];
-		if (this.intersects(basket)) {
-			basket.score++;
+		if (this.intersects(basket) && this.lane === basket.lane) {
+			basket.score += this.score;
 			OBJ.remove(this.id);
 		}
 	}
 	render() {
-		// draw apple
-		// Draw.image(this.imageName, this.x, this.y);
-		Draw.circle(this.x, this.y, 40);
+		Draw.image(this.imageName, this.x, this.y);
+		if (Debug.mode > 0)
+			Draw.circle(this.x, this.y, this.range, true);
 	}
 }
 
@@ -64,8 +65,9 @@ class Basket {
 			this.moveRight();
 	}
 	render() {
-		// Draw.image('basket', this.x, this.y);
-		Draw.circle(this.x, this.y, 40);
+		Draw.image('basket', this.x, this.y);
+		if (Debug.mode > 0)
+			Draw.circle(this.x, this.y, this.range, true);
 	}
 }
 
@@ -74,6 +76,12 @@ OBJ.addLink('Basket', Basket);
 
 let spawnTime, LANE_W;
 
+Loader.loadImage(Vec2.center, 'fresh', 'fresh.png');
+Loader.loadImage(Vec2.center, 'basket', 'basket.png');
+Loader.loadImage(Vec2.center, 'rotten', 'rotten.png');
+
+Font.setFamily('Montserrat Alternates, sans-serif');
+
 NZ.start({
 
 w: 360,
@@ -81,10 +89,13 @@ h: 640,
 
 bgColor: BGColor.sky,
 
+debugModeAmount: 2,
+
 start() {
 	LANE_W = Stage.w * 0.25;
 
-	OBJ.create('Basket', 0);
+	const n = OBJ.create('Basket', 0);
+	n.nzDepth = 1;
 
 	spawnTime = 0;
 
@@ -100,10 +111,14 @@ render() {
 },
 
 renderUI() {
-	Draw.textBGi(0, 0, Time.FPS);
-	Draw.textBGi(0, 1, OBJ.takeFrom('Basket')[0].score);
-}
+	Draw.setFont(Font.xxlb);
+	Draw.setHVAlign(Align.r, Align.t);
+	for (let i = 0; i < 2; i++) {
+		Draw.setFill(i > 0? C.white : C.black);
+		Draw.textRegular(Stage.w - 10 - i, 10 - 2*i, OBJ.takeFrom('Basket')[0].score);
+	}
+},
 
-
+embedGoogleFonts: ['Montserrat Alternates']
 
 });
