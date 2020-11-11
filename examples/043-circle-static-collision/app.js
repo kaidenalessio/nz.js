4f33ac051adf22a0603f2597e91d38afd6ed7f1e
kaiden <kaidenalessio@gmail.com>
2020-11-11 07:18:21 +0700

    fix overlap doubles, add constraint

--- examples/043-circle-static-collision/app.js
+++ examples/043-circle-static-collision/app.js
@@ -17,11 +17,11 @@ Global.circleIntersects = (dx, dy, rr) => dx*dx + dy*dy <= rr*rr;
 NZ.start({
 	start() {
 		Global.selected = null;
-		// OBJ.pushRaw('Ball', new Ball(Stage.w * 0.25, Stage.mid.h));
-		// OBJ.pushRaw('Ball', new Ball(Stage.w * 0.75, Stage.mid.h));
-		Utils.repeat(100, () => {
+		Global.checkCollision = true;
+		Utils.repeat(200, () => {
 			OBJ.pushRaw('Ball', new Ball(Stage.randomX, Stage.randomY, Mathz.range(10, 40)));
 		});
+		OBJ.pushRaw('Ball', new Ball(Stage.randomX, Stage.randomY, 100));
 	},
 	render() {
 
@@ -37,7 +37,7 @@ NZ.start({
 
 		if (Input.mouseHold(0)) {
 			if (Global.selected) {
-				Tween.lerp(Global.selected, { x: Input.mouseX, y: Input.mouseY }, 0.2);
+				Tween.lerp(Global.selected, { x: Input.mouseX, y: Input.mouseY }, 0.5);
 			}
 		}
 
@@ -45,36 +45,58 @@ NZ.start({
 			Global.selected = null;
 		}
 
-		let dx, dy, dist, overlap, ox, oy;
-		for (const ball of OBJ.takeRaw('Ball')) {
-			for (const other of OBJ.takeRaw('Ball')) {
-				if (ball.id !== other.id) {
-					dx = other.x - ball.x;
-					dy = other.y - ball.y;
-					rr = ball.r + other.r;
-					if (Global.circleIntersects(dx, dy, rr)) {
-						// calculate overlap
-						dist = Math.sqrt(dx*dx + dy*dy);
-						overlap = rr - dist;
-						ox = overlap * dx / dist;
-						oy = overlap * dy / dist;
+		if (Input.keyDown(KeyCode.Space)) Global.checkCollision = !Global.checkCollision;
 
-						// displace
-						ball.x -= ox;
-						ball.y -= oy;
-						other.x += ox;
-						other.y += oy;
+		if (Global.checkCollision) {
+			let dx, dy, dist, overlap, ox, oy;
+			for (const ball of OBJ.takeRaw('Ball')) {
+				for (const other of OBJ.takeRaw('Ball')) {
+					if (ball.id !== other.id) {
+						dx = other.x - ball.x;
+						dy = other.y - ball.y;
+						rr = ball.r + other.r;
+						if (Global.circleIntersects(dx, dy, rr)) {
+							// calculate overlap
+							dist = Math.sqrt(dx*dx + dy*dy);
+							overlap = (rr - dist) * 0.5;
+							ox = overlap * dx / dist;
+							oy = overlap * dy / dist;
+
+							// displace
+							ball.x -= ox;
+							ball.y -= oy;
+							other.x += ox;
+							other.y += oy;
+						}
 					}
 				}
 			}
 		}
 
+		for (const ball of OBJ.takeRaw('Ball')) {
+			// constraint
+			if (ball.x > Stage.w - ball.r) {
+				ball.x = Stage.w - ball.r;
+			}
+			if (ball.x < ball.r) {
+				ball.x = ball.r;
+			}
+			if (ball.y > Stage.h - ball.r) {
+				ball.y = Stage.h - ball.r;
+			}
+			if (ball.y < ball.r) {
+				ball.y = ball.r;
+			}
+		}
+
 		Draw.setAlpha(0.5);
 		for (const ball of OBJ.takeRaw('Ball')) {
-			Draw.setFill(ball.c);
+			Draw.setFill(Global.checkCollision? ball.c : C.white);
 			Draw.circle(ball.x, ball.y, ball.r);
 		}
 		Draw.resetAlpha();
+
+		Draw.textBGi(0, 0, `Press space to ${Global.checkCollision? 'disable' : 'enable'} collision.`);
 	},
 	bgColor: C.black
 });
