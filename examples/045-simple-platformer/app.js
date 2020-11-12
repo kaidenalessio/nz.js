c5707cecd0d4fdab9c92d50570adc7d3b5b40d69
kaiden <kaidenalessio@gmail.com>
2020-11-12 15:40:51 +0700

    squishy animation

--- examples/045-simple-platformer/app.js
+++ examples/045-simple-platformer/app.js
@@ -11,7 +11,7 @@ class Block {
 		this.bottom = this.y + this.h;
 	}
 	intersects(p) {
-		return (p.right >= this.left && p.left <= this.right && p.bottom >= this.top && p.top < this.bottom);
+		return (p.right >= this.left && p.left <= this.right && p.bottom >= this.top && p.top <= this.bottom);
 	}
 	render() {
 		Draw.rect(this.x, this.y, this.w, this.h);
@@ -23,12 +23,22 @@ class Player {
 	constructor(x, y, w, h) {
 		this.x = x;
 		this.y = y;
-		this.w = w;
-		this.h = h;
+		this.wd = w; // width draw
+		this.hd = h; // height draw
+		this.w = this.wd * 0.8; // hitbox width (can be larger or smaller)
+		this.h = this.hd; // hitbox height (can be larger or smaller)
+		this.mid = { // half dimensions
+			wd: this.wd * 0.5,
+			hd: this.hd * 0.5,
+			w: this.w * 0.5,
+			h: this.h * 0.5
+		};
 		this.xp = this.x;
 		this.yp = this.y;
 		this.vx = 0;
 		this.vy = 0;
+		this.xs = 1; // image x scale
+		this.ys = 1; // image y scale
 		this.acc = 1.5;
 		this.bounce = -0.3;
 		this.vxlimit = 8;
@@ -39,10 +49,10 @@ class Player {
 		this.grounded = false;
 		this.jumpforce = -20;
 		this.airfriction = 0.99;
-		this.top = this.y - this.h * 0.5;
-		this.left = this.x - this.w * 0.5;
-		this.right = this.x + this.w * 0.5;
-		this.bottom = this.y + this.h * 0.5;
+		this.top = this.y - this.mid.h;
+		this.left = this.x - this.mid.w;
+		this.right = this.x + this.mid.w;
+		this.bottom = this.y + this.mid.h;
 	}
 	update() {
 
@@ -90,11 +100,19 @@ class Player {
 		this.x += this.vx;
 		this.y += this.vy;
 
-		// origin center middle
-		this.top = this.y - this.h * 0.5;
-		this.left = this.x - this.w * 0.5;
-		this.right = this.x + this.w * 0.5;
-		this.bottom = this.y + this.h * 0.5;
+		// origin center bottom
+		this.top = this.y - this.h;
+		this.left = this.x - this.mid.w;
+		this.right = this.x + this.mid.w;
+		this.bottom = this.y;
+	}
+	squish() {
+		this.xs = 0.9;
+		this.ys = 1 / this.xs;
+	}
+	squishTop(vy) {
+		this.ys = 1 - 0.01 * vy;
+		this.xs = 1 / this.ys;
 	}
 	constraint() {
 		this.grounded = false;
@@ -106,38 +124,49 @@ class Player {
 			// only check if we intersect
 			if (b.intersects(this)) {
 				// if we came from left
-				if (this.vx > 0 && this.xp <= b.left - this.w * 0.5) {
-					this.x = b.left - this.w * 0.5;
+				if (this.vx > 0 && this.xp <= b.left - this.mid.w) {
+					this.x = b.left - this.mid.w;
 					this.vx *= this.bounce;
+					this.squish();
 				}
 				// if we came from right
-				if (this.vx < 0 && this.xp >= b.right + this.w * 0.5) {
-					this.x = b.right + this.w * 0.5;
+				if (this.vx < 0 && this.xp >= b.right + this.mid.w) {
+					this.x = b.right + this.mid.w;
 					this.vx *= this.bounce;
+					this.squish();
 				}
 				// if we came from above
-				if (this.vy > 0 && this.yp <= b.top - this.h * 0.5) {
-					this.y = b.top - this.h * 0.5;
+				if (this.vy > 0 && this.yp <= b.top) {
+					this.y = b.top;
+					if (this.vy > this.gravity) this.squishTop(this.vy);
 					this.vy *= this.bounce;
 					this.grounded = true;
 				}
 				// if we came from below
-				if (this.vy < 0 && this.yp >= b.bottom + this.h * 0.5) {
-					this.y = b.bottom + this.h * 0.5;
+				if (this.vy < 0 && this.yp >= b.bottom + this.h) {
+					this.y = b.bottom + this.h;
 					this.vy *= this.bounce;
 				}
 			}
 		}
 
 		// ground check
-		if (this.y > Stage.h - 100 - this.h * 0.5) {
-			this.y = Stage.h - 100 - this.h * 0.5;
+		if (this.y > Global.getGroundY()) {
+			this.y = Global.getGroundY();
+			if (this.vy > this.gravity) this.squishTop(this.vy);
 			this.vy *= this.bounce;
 			this.grounded = true;
 		}
 	}
 	render() {
-		Draw.rectRotated(this.x, this.y, this.w, this.h);
+
+		// update squish
+		this.xs += (1 - this.xs) * 0.2;
+		this.ys += (1 - this.ys) * 0.2;
+
+		Draw.onTransform(this.x, this.y, this.xs, this.ys, 0, () => {
+			Draw.rect(-this.mid.wd, -this.hd, this.wd, this.hd);
+		});
 	}
 }
 
@@ -150,10 +179,14 @@ NZ.start({
 	},
 	start() {
 		OBJ.rawClearAll();
-		const n = OBJ.rawPush('Player', new Player(Stage.randomX, 100, Mathz.range(32, 128), Mathz.range(32, 128)));
+		const n = OBJ.rawPush('Player', new Player(Stage.randomX, 100, 32, 48));
 		OBJ.rawPush('Block', new Block(Stage.mid.w, Global.getGroundY() - (80 + n.h), 100, 80));
 		OBJ.rawPush('Block', new Block(Stage.mid.w - (100 + n.w), Global.getGroundY() - 50, 100, 50));
 		OBJ.rawPush('Block', new Block(Stage.mid.w + (100 + n.w), Global.getGroundY() - 50, 100, 50));
+
+		for (let i = 0, w=n.wd, h=n.hd, cols=Stage.w/w, rows=Stage.h/h; i < 50; i++) {
+			OBJ.rawPush('Block', new Block(Mathz.irange(cols) * w, Mathz.irange(rows) * h, w, h));
+		}
 	},
 	render() {
 
