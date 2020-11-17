c0c2838d4117b528dd7c0d138e139ae3f447e1ac
kaiden <kaidenalessio@gmail.com>
2020-11-17 11:16:13 +0700

    Swapping routine

--- examples/049-match-3/app.js
+++ examples/049-match-3/app.js
@@ -1,18 +1,20 @@
 class M3Cell {
-	static COLORS = [C.red, C.yellow, C.green, C.blue];
+	static COLORS = [C.orangeRed, C.gold, C.limeGreen, C.dodgerBlue];
 	constructor(grid, i, j, x, y) {
 		this.grid = grid;
 		this.i = i;
 		this.j = j;
 		this.x = x;
 		this.y = y;
-		this._c = C.random();//Utils.pick(M3Cell.COLORS);
+		this.data = {
+			color: Utils.pick(M3Cell.COLORS)
+		};
 	}
 	render() {
-		Draw.setColor(this._c, C.black);
-		Draw.circle(this.x + this.grid.midSize, this.y + this.grid.midSize, this.grid.midSize);
+		Draw.setColor(this.data.color, C.black);
+		Draw.circle(this.x + this.grid.midSize, this.y + this.grid.midSize, this.grid.midSize - 1);
 		Draw.stroke();
-		Draw.rect(this.x, this.y, this.grid.size, this.grid.size, true);
+		// Draw.rect(this.x, this.y, this.grid.size, this.grid.size, true);
 	}
 }
 
@@ -39,7 +41,7 @@ class M3Grid {
 	fill() {
 		for (let j = 0; j < this.rows; j++) {
 			for (let i = 0; i < this.cols; i++) {
-				let b = this.toWorld(i, j);
+				const b = this.toWorld(i, j);
 				this.cells[this.getIndex(i, j)] = new M3Cell(this, i, j, b.x, b.y);
 			}
 		}
@@ -74,6 +76,17 @@ class M3Grid {
 	getCell(i, j) {
 		return this.cells[this.getIndex(i, j)];
 	}
+	drawCell(c, isStroke) {
+		const b = Global.grid.toWorld(c.i, c.j);
+		Draw.rect(b.x, b.y, this.size, this.size, isStroke);
+	}
+	swap(i, j, ii, jj) {
+		// make sure both are exists
+		// cuz there is no check here
+		i = this.getCell(i, j);
+		j = this.getCell(ii, jj);
+		[i.data, j.data] = [j.data, i.data];
+	}
 	render() {
 		for (let i = 0; i < this.cells.length; i++) {
 			this.cells[i].render();
@@ -84,16 +97,46 @@ class M3Grid {
 NZ.start({
 	w: 360,
 	h: 640,
-	bgColor: C.orchid,
+	bgColor: BGColor.cream,
 	stylePreset: StylePreset.noGapCenter,
 	init() {
 		Global.grid = new M3Grid(Stage.w, Stage.h, 40);
+		Global.selected = null;
+		Global.mouseGrid = null;
 	},
 	start() {
 		Global.grid.init();
 		Global.grid.fill();
+		Global.mouseGrid = Global.grid.toGridFloor(Input.mouseX, Input.mouseY);
+	},
+	update() {
+		Global.mouseGrid = Global.grid.toGridFloor(Input.mouseX, Input.mouseY);
+		if (Input.mouseDown(0) || Input.mouseUp(0)) {
+			const b = Global.grid.getCell(Global.mouseGrid.i, Global.mouseGrid.j);
+			if (b) {
+				if (Global.selected) {
+					let di = Math.abs(b.i - Global.selected.i),
+						dj = Math.abs(b.j - Global.selected.j);
+
+					if (di + dj === 1) {
+						Global.grid.swap(Global.selected.i, Global.selected.j, b.i, b.j);
+					}
+
+					Global.selected = null;
+				}
+				else {
+					Global.selected = b;
+				}
+			}
+		}
 	},
 	render() {
 		Global.grid.render();
+		Draw.setStroke(C.black);
+		Global.grid.drawCell(Global.mouseGrid, true);
+		if (Global.selected) {
+			Draw.setFill(C.makeRGBA(0, 0.5));
+			Global.grid.drawCell(Global.selected);
+		}
 	}
 });
