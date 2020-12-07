88c3de9cbed6b8adaa138999024320fd16f45d82
kaiden <kaidenalessio@gmail.com>
2020-12-07 14:44:55 +0700

    player movement and shoot

--- examples/055-snow-shooter/app.js
+++ examples/055-snow-shooter/app.js
@@ -61,6 +61,25 @@ Manager.setup({
 			this.baddies = this.baddies || {};
 			this.baddies[tag] = baddieConstructor;
 		},
+		cloneBaddie(tag) {
+			// not deep, child object will not be cloned
+			const baddie = {};
+			for (const key in this.baddies[tag]) {
+				const value = this.baddies[tag][key];
+				if (value instanceof Function) {
+					baddie[key] = value.bind(baddie);
+				}
+				else {
+					baddie[key] = value;
+				}
+			}
+			return baddie;
+		},
+		spawnEnemy(baddieTag, x, y) {
+			const n = this.cloneBaddie(baddieTag);
+			this.enemies.push(n);
+			return n;
+		},
 		pointOutsideStage(p) {
 			const rect = {
 				x: 0,
@@ -70,14 +89,14 @@ Manager.setup({
 			};
 			return !rectContainsPoint(rect, p);
 		},
-		drawRect(rect, originX=0, originY=0, isStroke=false) {
-			const x = rect.x - rect.w * originX;
-			const y = rect.y - rect.h * originY;
-			Draw.rect(x, y, rect.w, rect.h, isStroke);
+		drawRectRotated(rect, angle, isStroke=false) {
+			Draw.roundRectRotated(rect.x, rect.y, rect.w, rect.h, 10, Mathz.radtodeg(angle), isStroke);
+			const head = Math.polar(rect.x, rect.y, rect.w / 2, angle);
+			return head;
 		},
 		play() {
 			this.player.x = Stage.mid.w;
-			this.player.y = this.groundY;
+			this.player.y = Stage.mid.h;
 
 			this.isStarting = false;
 			this.isPlaying = true;
@@ -87,12 +106,18 @@ Manager.setup({
 			this.isGameOver = true;
 		},
 		shoot() {
+			const head = Math.polar(this.player.x, this.player.y, this.player.w / 2, this.player.shootAngle);
 			const n = {
-				x: this.player.x,
-				y: this.player.y,
-				direction: this.player.direction
+				x: head.x,
+				y: head.y,
+				direction: this.player.shootAngle
 			};
 			this.bullets.push(n);
+		},
+		moveForward(from, to, speed) {
+			const polar = Math.polar(0, 0, speed, Math.angleBetween(from.x, from.y, to.x, to.y));
+			from.x += polar.x;
+			from.y += polar.y;
 		}
 	},
 	onInit() {
@@ -100,10 +125,9 @@ Manager.setup({
 		Time.clampedDeltaTime = 0;
 		this.isMuted = false;
 		this.bullets = [];
-		this.groundY = Stage.h - 64;
 
 		// --- Buttons ---
-		this.createButton(Stage.mid.w, Stage.mid.h, 200, 50, 'Start Game', () => Manager.play(), () => Manager.isStarting);
+		this.createButton(Stage.mid.w, Stage.mid.h, 200, 50, 'Start Game', () => { if (this.isStarting) Manager.play(); }, () => Manager.isStarting);
 
 		// --- Main Player ---
 		this.player = {
@@ -111,19 +135,27 @@ Manager.setup({
 			y: 0,
 			w: 32,
 			h: 32,
+			speed: 5,
+			shootTime: 0,
 			direction: 0,
-			bulletSpeed: 5
+			shootAngle: 0,
+			bulletSpeed: 10,
+			shootInterval: 5
 		};
 
 		// --- Baddies/Enemy Creatures ---
-		this.createBaddie('fly', {
+		this.createBaddie('zombie', {
 			x: 0,
 			y: 0,
-			w: 20,
-			h: 12,
-			color: C.black,
+			w: 24,
+			h: 24,
+			color: C.red,
+			speed: 5,
 			moveTime: -1,
-			movement(moveTime) {
+			moveCycle(moveTime) {
+				if (this.moveTime > 20) {
+					this.moveForward(this, this.player, this.speed);
+				}
 				if (this.moveTime === 0) {
 				}
 				else if (this.moveTime === 0) {
@@ -133,8 +165,6 @@ Manager.setup({
 				}
 			}
 		});
-		this.createBaddie('bunny');
-		this.createBaddie('elephant');
 
 		this.enemies = []; // store baddies
 	},
@@ -151,7 +181,7 @@ Manager.setup({
 		if (this.isPlaying) {
 			for (const enemy of this.enemies) {
 				if (enemy.moveTime < 0) enemy.moveTime = 0;
-				enemy.movement(Math.floor(enemy.moveTime));
+				enemy.moveCycle(Math.floor(enemy.moveTime));
 				if (enemy.moveTime < 0) enemy.moveTime = 0;
 				else this.moveTime += Time.clampedDeltaTime;
 			}
@@ -159,22 +189,47 @@ Manager.setup({
 
 		// --- Player ---
 		if (this.isPlaying) {
-			if (Input.keyHold(KeyCode.Left)) {
-				this.player.x -= 5;
-				this.player.direction = Math.PI;
+			const nextPos = {
+				x: 0,
+				y: 0
+			};
+			if (Input.keyHold(KeyCode.Left) || Input.keyHold(KeyCode.A)) {
+				nextPos.x -= this.player.speed;
 			}
-			if (Input.keyHold(KeyCode.Right)) {
-				this.player.x += 5;
-				this.player.direction = 0;
+			if (Input.keyHold(KeyCode.Right) || Input.keyHold(KeyCode.D)) {
+				nextPos.x += this.player.speed;
 			}
-			if (Input.keyHold(KeyCode.Up)) {
-				this.player.direction = Math.PI * 1.5;
+			if (Input.keyHold(KeyCode.Up) || Input.keyHold(KeyCode.W)) {
+				nextPos.y -= this.player.speed;
 			}
-			if (Input.keyHold(KeyCode.Down)) {
-				this.player.direction = Math.PI * 0.5;
+			if (Input.keyHold(KeyCode.Down) || Input.keyHold(KeyCode.S)) {
+				nextPos.y += this.player.speed;
 			}
-			if (Input.keyDown(KeyCode.Z)) {
+			if (nextPos.x !== 0 || nextPos.y !== 0) {
+				this.player.direction = Math.angleBetween(0, 0, nextPos.x, nextPos.y);
+				const polar = Math.polar(0, 0, this.player.speed, this.player.direction);
+				this.player.x += polar.x;
+				this.player.y += polar.y;
+			}
+			if (this.player.x > Stage.w) {
+				this.player.x = Stage.w;
+			}
+			if (this.player.x < 0) {
+				this.player.x = 0;
+			}
+			if (this.player.y > Stage.h) {
+				this.player.y = Stage.h;
+			}
+			if (this.player.y < 0) {
+				this.player.y = 0;
+			}
+			this.player.shootAngle = Math.angleBetween(this.player.x, this.player.y, Input.mouseX, Input.mouseY);
+			if (this.player.shootTime < 0 && this.getMouseHold()) {
+				this.player.shootTime = this.player.shootInterval;
 				this.shoot();
+			}
+			else {
+				this.player.shootTime -= Time.clampedDeltaTime;
 			}
 		}
 
@@ -184,8 +239,29 @@ Manager.setup({
 			const polar = Math.polar(b.x, b.y, this.player.bulletSpeed, b.direction);
 			b.x = polar.x;
 			b.y = polar.y;
-			if (this.pointOutsideStage(b)) {
-				this.bullets.splice(i, 0);
+			let isHitting = false;
+			for (const enemy of this.enemies) {
+				const hitbox = {
+					x: enemy.x - enemy.w / 2,
+					y: enemy.y - enemy.h / 2,
+					w: enemy.w,
+					h: enemy.h
+				};
+				if (rectContainsPoint(hitbox, b)) {
+					enemy.isDead = true;
+					isHitting = true;
+					break;
+				}
+			}
+			if (isHitting || this.pointOutsideStage(b)) {
+				this.bullets.splice(i, 1);
+			}
+		}
+
+		// --- Removing Enemy ---
+		for (let i = this.enemies.length - 1; i >= 0; --i) {
+			if (this.enemies[i].isDead) {
+				this.enemies.splice(i, 1);
 			}
 		}
 
@@ -194,14 +270,33 @@ Manager.setup({
 
 		// --- Player ---
 		if (this.isPlaying) {
-			Draw.setColor(C.orchid);
-			this.drawRect(this.player, 0.5, 1);
+			Draw.setColor(C.white);
+			const head = this.drawRectRotated(this.player, this.player.shootAngle);
+			Draw.setColor(C.black);
+			Draw.circle(head.x, head.y, 4);
+		}
+
+		// --- Enemy ---
+		if (this.isPlaying) {
+			for (const enemy of this.enemies) {
+				Draw.setColor(enemy.color);
+				this.drawRectRotated(enemy, enemy.direction);
+			}
+		}
+
+		// --- Bullet ---
+		if (this.isPlaying) {
+			Draw.setColor(C.white);
+			for (const bullet of this.bullets) {
+				Draw.circle(bullet.x, bullet.y, 4);
+			}
 		}
 
 		// --- Starting Page ---
 		if (this.isStarting) {
 			// Title
 			Draw.setFont(Font.xxlb);
+			Draw.setColor(C.black);
 			Draw.setHVAlign(Align.c, Align.t);
 			Draw.text(Stage.mid.w, Stage.h * 0.1, 'SNOW SHOOTER');
 		}
